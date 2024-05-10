e3bdcd8482b49af0cda81daf2c483b2e0bb5c749
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-10 03:35:10 -0600

    Added function loadContent to handle loading video titles in content container, and reorganized triggers and functions for better code readability

--- webscrapper/popup.js
+++ webscrapper/popup.js
@@ -1,3 +1,78 @@
+/* Starting the extension */
+
+document.addEventListener('DOMContentLoaded', async function() {
+    // Load skeleton on launch
+    loadSkeleton();
+
+    const response = await fetch('http://localhost:3000/preset?SetPreset=false');
+
+    const preset = await response.json();
+
+    const searchValue = preset[0];
+    const videoTitles = preset[1];
+
+    document.getElementById('searchBar').value = searchValue;
+
+    loadContents(videoTitles);
+});
+
+/* Launch Search */
+
+// Check search button is clicked
+document.getElementById("searchButton").addEventListener('click', launchSearch);
+
+// Check if enter is pressed
+document.getElementById('searchBar').addEventListener('keydown', handleEnterPress);
+
+/* Menu Bar triggers */
+
+// Activate menu bar
+var isFooterDisplayed = false; // variable to check if button is clicked
+document.getElementById("displayFooter").addEventListener('click', function() {
+        const footer = document.getElementById("footer"); // Get footer element
+        
+        // Display footer if not displayed
+        if (!isFooterDisplayed) {
+            isFooterDisplayed = true;
+            footer.style.bottom = "0px";
+
+        // Hide footer if displayed
+        } else {
+            footer.style.bottom = "-280px";
+            isFooterDisplayed = false;
+        }
+});
+
+
+/* Helper functions */
+
+// Load contents
+function loadContents(videoTitles) {
+    if (videoTitles !== null) {
+        const resultsElement = document.getElementById('mainContent');
+
+        resultsElement.innerHTML = '';
+
+        videoTitles.forEach(title => {
+            
+            let youtubeTitle = document.createElement('div');
+            youtubeTitle.className = 'youtube-title';
+
+            let anchor = document.createElement('a');
+
+            anchor.className = 'actual-content';
+            anchor.innerHTML = (title[2] !== null) ? title[0].slice(0, 66) : title[0].slice(0, 50) + ' [No link]';
+            anchor.href = title[2];
+            anchor.title = title[1] || 'No description available';
+            anchor.target = '_blank';
+
+            youtubeTitle.appendChild(anchor);
+            
+            resultsElement.appendChild(youtubeTitle);
+        });
+    }
+}
+
 // Load skeleton
 function loadSkeleton() {
     const grid = document.querySelector('div.main-content');
@@ -44,85 +119,9 @@ async function launchSearch() {
     const response = await fetch(`http://localhost:3000/search?Query=${searchQuery}`);
     const videoTitles = await response.json();
 
-    resultsElement.innerHTML = '';
-
-    videoTitles.forEach(title => {
-        let youtubeTitle = document.createElement('div');
-        youtubeTitle.className = 'youtube-title';
-
-        let anchor = document.createElement('a');
-        anchor.className = 'actual-content';
-        anchor.innerHTML = (title[2] !== null) ? title[0].slice(0, 66) : title[0].slice(0, 50) + ' [No link]';
-        anchor.href = title[2];
-        anchor.title = title[1] || 'No description available';
-        anchor.target = '_blank';
-
-        youtubeTitle.appendChild(anchor);
-        
-        resultsElement.appendChild(youtubeTitle);
-    });
+    loadContents(videoTitles);
 
     // Add triggers once search is done loading
     document.getElementById("searchButton").addEventListener('click', launchSearch);
     document.getElementById('searchBar').addEventListener('keydown', handleEnterPress);
 }
-
-// Load skeleton on launch
-loadSkeleton();
-
-// Starting the extension
-document.addEventListener('DOMContentLoaded', async function() {
-    const response = await fetch('http://localhost:3000/preset?SetPreset=false');
-
-    const preset = await response.json();
-
-    const searchValue = preset[0];
-    const videoTitles = preset[1];
-
-    if (videoTitles !== null) {
-        const resultsElement = document.getElementById('mainContent');
-
-        resultsElement.innerHTML = '';
-
-        videoTitles.forEach(title => {
-            document.getElementById('searchBar').value = searchValue;
-            let youtubeTitle = document.createElement('div');
-            youtubeTitle.className = 'youtube-title';
-
-            let anchor = document.createElement('a');
-
-            anchor.className = 'actual-content';
-            anchor.innerHTML = (title[2] !== null) ? title[0].slice(0, 66) : title[0].slice(0, 50) + ' [No link]';
-            anchor.href = title[2];
-            anchor.title = title[1] || 'No description available';
-            anchor.target = '_blank';
-
-            youtubeTitle.appendChild(anchor);
-            
-            resultsElement.appendChild(youtubeTitle);
-        });
-    }
-});
-
-// Trigger to search results
-document.getElementById("searchButton").addEventListener('click', launchSearch);
-
-// Check if enter is pressed when searching
-document.getElementById('searchBar').addEventListener('keydown', handleEnterPress);
-
-// Displayinng the footer
-var isFooterDisplayed = false; // variable to check if button is clicked
-document.getElementById("displayFooter").addEventListener('click', function() {
-        const footer = document.getElementById("footer"); // Get footer element
-        
-        // Display footer if not displayed
-        if (!isFooterDisplayed) {
-            isFooterDisplayed = true;
-            footer.style.bottom = "0px";
-
-        // Hide footer if displayed
-        } else {
-            footer.style.bottom = "-280px";
-            isFooterDisplayed = false;
-        }
-});
